c511415c2b5c3453dec5d48cceca0cec84aa2540
johnnydwtt <johnnydwtt@gmail.com>
2021-07-12 16:27:16 +0200

    ex5

---
+++ partie1/assets/js/exercice5.js
@@ -0,0 +1,13 @@
+// variables
+var firstNumber = Number
+var secondNumber = Number
+var resultNumber = Number
+
+// instructions
+
+firstNumber = prompt("premier chiffre?")
+secondNumber = prompt("second chiffre?")
+
+resultNumber = Number(firstNumber) + Number(secondNumber);
+
+alert(`Voici le résultat de ton addition ${resultNumber}`)
