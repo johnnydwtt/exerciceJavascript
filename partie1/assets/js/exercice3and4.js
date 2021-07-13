aed19ab56866387b73db97946fd5e763608ea3b5
johnnydwtt <johnnydwtt@gmail.com>
2021-07-13 09:03:58 +0200

    exo2 part2

--- partie1/assets/js/exercice3and4.js
+++ partie1/assets/js/exercice3and4.js
@@ -2,11 +2,14 @@
 var userName = String
 var age = Number
 var city = String
+var mail = String
 
 // Questions
 userName = prompt("Comment t'appelles-tu ?")
 age = prompt("Quel age as-tu ?")
+mail = prompt("Ton adresse email?")
 city = prompt("Tu habites ou ?")
 
+
 // boites d'alert
-alert(`Bonjour, je m'appelle ${userName} j'ai actuellement ${age} ans et j'habite à ${city}`);
+alert(`Bonjour, je m'appelle ${userName} | mail : ${mail} | age: ${age} ans | ville de naissance: ${city}`);
---
+++ partie2/assets/js/exercice2.js
@@ -0,0 +1,8 @@
+var age
+
+age = prompt ("Quel âge avez-vous?")
+
+    if (age < 18)
+        alert(`Vous êtes mineure vous avez ${age} ans`)
+    else if (age >= 18)
+        alert(`Vous êtes majeure vous avez ${age} ans`)
