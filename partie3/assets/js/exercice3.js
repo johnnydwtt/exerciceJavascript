74e01a635bf01f89abcad264f64ae8cd4658e73e
johnnydwtt <johnnydwtt@gmail.com>
2021-07-13 15:17:22 +0200

    exercice2 part3

---
+++ partie3/assets/js/exercice3.js
@@ -0,0 +1,8 @@
+let languages = ['html', 'css', 'javascript', 'php', 'mysql', 'c++','ruby','python'];
+
+for (var i = 0; i < languages.length; i++){
+
+    document.write(languages[i]+" | ");
+
+}
+
