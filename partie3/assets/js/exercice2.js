e3f9de185185c48d3049bcdc67415ed5bd12a2d8
johnnydwtt <johnnydwtt@gmail.com>
2021-07-13 14:11:49 +0200

    exercice2 part3

---
+++ partie3/assets/js/exercice2.js
@@ -0,0 +1,15 @@
+let infinityNum = 0;
+
+while (infinityNum<=100){
+    if(infinityNum%15){
+         document.write(infinityNum+' | ');  
+    }
+    else{
+          document.write('... | ');
+    }
+    infinityNum++;
+}
+
+
+     
+
